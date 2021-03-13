c3e64160d6858e87bf702ec9c38a1a046668b2b0
Rayan Castro <rayancdc@gmail.com>
2021-03-13 16:01:31 +0400

    Add show page to lectures administration

--- app/controllers/lectures_controller.rb
+++ app/controllers/lectures_controller.rb
@@ -1,4 +1,13 @@
 class LecturesController < ApplicationController
+  before_action :set_lecture, only: [:show, :edit, :update, :destroy]
+  def index
+    @lectures = policy_scope(Lecture)
+  end
+
+  def show
+    authorize @lecture
+  end
+
   def new
     @lecture = Lecture.new
     authorize @lecture
@@ -16,7 +25,27 @@ class LecturesController < ApplicationController
     end
   end
 
+  def edit
+    authorize @lecture
+  end
+
+  def update
+    authorize @lecture
+
+    if @lecture.update(lecture_params)
+      redirect_to lectures_path
+    else
+      flash[:alert] = Rails.env.development? ? @lecture.errors.full_messages : "Lecture could not be created"
+      render :edit
+    end
+  end
+
+
   private
+
+  def set_lecture
+    @lecture = Lecture.find(params[:id])
+  end
 
   def lecture_params
     params.require(:lecture).permit(:name, :description, :teachers, :start_date, :duration, :location, :max_students, :status)
--- app/models/lecture.rb
+++ app/models/lecture.rb
@@ -1,6 +1,6 @@
 class Lecture < ApplicationRecord
   has_many :registrations
-  has_many :students, through: :registrations, class_name: 'User'
+  has_many :students, through: :registrations, source: :user
 
   validates :name, :start_date, :duration, :location, presence: true
 end
